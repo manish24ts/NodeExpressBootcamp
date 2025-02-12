135aa2e69d25f79b935aa0a17efc49a503313f7c
manish24ts <126053896+manish24ts@users.noreply.github.com>
2025-02-13 00:19:12 +0530

    Update solution4.js

--- Task3/solution4.js
+++ Task3/solution4.js
@@ -7,24 +7,24 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-var bandName = "";
+var result = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function bandNameGenerator(req, res, next) {
+function stringaddition(req, res, next) {
   console.log(req.body);
-  bandName = req.body["street"] + req.body["pet"];
+  result = req.body["string1"] + req.body["string2"];
   next();
 }
 
-app.use(bandNameGenerator);
+app.use(stringaddition);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
-  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+  res.send(`<h1>Your concatenated string is:</h1><h2>${result}✌️</h2>`);
 });
 
 app.listen(port, () => {
